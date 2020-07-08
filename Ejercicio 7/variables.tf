5e529840e57446a202bef06d73faa53fa1b0bb47
frisisuas <44669926+frisisuas@users.noreply.github.com>
2020-07-08 12:12:10 +0200

    Update variables.tf

--- Ejercicio 7/variables.tf
+++ Ejercicio 7/variables.tf
@@ -10,6 +10,7 @@ variable "password" {
   type = string
   default = "P4$$w0rd12345"
 }
+#-------añadimos la variable ssh con el contenido de la clave publica----
 variable "ssh" {
   type = string
   default = "<substituir por ssh-rsa ... loquesea@email.com de tu clave publica>"
